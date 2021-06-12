4c3a149f15ca6fad1546ea8ac947fe4bfa6731d1
Tdaycode <Tayorgudestryhero@gmail.com>
2021-06-12 22:43:45 +0100

    Auth joi validation

---
+++ validations/auth.validation.js
@@ -0,0 +1,38 @@
+const Joi = require('joi');
+const { password } = require('./custom.validation');
+
+const register = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().custom(password),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    phone: Joi.string().required()
+  }),
+};
+
+const login = {
+  body: Joi.object().keys({
+    login: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+};
+
+const logout = {
+  body: Joi.object().keys({
+    refreshToken: Joi.string().required(),
+  }),
+};
+
+const refreshTokens = {
+  body: Joi.object().keys({
+    refreshToken: Joi.string().required(),
+  }),
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+  refreshTokens
+};
